9082416649a88c3ea4913bbac1337db47d2b2705
Mtimande23 <shongwethemba447@gmail.com>
2025-03-31 22:37:59 +0200

    LATEST  ON ASSIGNMENTS(ASSIGNMENT-TWO)
    The aim of Assignment 2 is to analyze the performance of an AVL Tree to determine whether it maintains balance and provides good search and insertion efficiency, regardless of dataset size.The program will read a dataset from GenericsKB.txt, insert it into an AVL tree, and process search queries from GenericsKB-queries.txt. To evaluate performance, instrumentation will be added to count key comparison operations during insertion and search, and experiments will be conducted with varying dataset sizes (up to 50,000) to compare empirical results with theoretical expectations. The results, including best, average, and worst-case scenarios, will be visualized using graphs(Given by plotter.py when ran) .

---
+++ ASSIGNMENT_TWO/docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"GenericsKbAVLAppPart1","l":"GenericsKbAVLAppPart1()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"GenericsKbAVLAppPart1","l":"load()"},{"p":"<Unnamed>","c":"GenericsKbAVLAppPart1","l":"main()"},{"p":"<Unnamed>","c":"GenericsKbAVLAppPart1","l":"Query()"}];updateSearchResults();
